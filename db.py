647dd1620ad2a964125d64f4c97f0148fdafeba9
yasamprom <stepa.strunkov@gmail.com>
2023-02-12 14:25:24 +0300

    local mongo with docker compose

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 import pymongo
+import logging
 
 
 async def add_user_to_db(user_id, unique=False):
-    client = pymongo.MongoClient("mongodb://root:pass@127.0.0.1:27017")
+    client = pymongo.MongoClient("mongodb://mongodb:27017/")
     db = client["users"]
     col = db["user_ids"]
     if unique:
@@ -13,7 +14,7 @@ async def add_user_to_db(user_id, unique=False):
 
 
 async def add_valentine(user_id):
-    client = pymongo.MongoClient("mongodb://root:pass@127.0.0.1:27017")
+    client = pymongo.MongoClient("mongodb://mongodb:27017/")
     db = client["users"]
     col = db["valentines"]
-    col.insert_one({"user_id": user_id})
+    col.insert_one({"author": user_id})
