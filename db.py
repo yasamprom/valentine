b689c2659d4554f2edd973ff1e15e0856e73457e
yasamprom <stepa.strunkov@gmail.com>
2023-02-12 15:47:06 +0300

    exceptions

--- db.py
+++ db.py
@@ -3,20 +3,31 @@ import logging
 
 
 async def add_user_to_db(user_id, unique=False):
-    client = pymongo.MongoClient("mongodb://mongodb:27017/")
-    db = client["users"]
-    col = db["user_ids"]
-    if unique:
-        x = col.find_one(user_id)
-        if x:
-            return
-    col.insert_one({"user_id": user_id})
-    client.close()
+    try:
+        client = pymongo.MongoClient("mongodb://mongodb:27017/")
+        db = client["users"]
+        col = db["user_ids"]
+        if unique:
+            x = col.find_one({"user_id": user_id})
+            if x:
+                return
+        col.insert_one({"user_id": user_id})
+
+        total = 0
+        for user in col.find():
+            total += 1
+        client.close()
+        return str(total)
+    except Exception as exc:
+        return str(exc)
 
 
 async def add_valentine(user_id):
-    client = pymongo.MongoClient("mongodb://mongodb:27017/")
-    db = client["users"]
-    col = db["valentines"]
-    col.insert_one({"author": user_id})
-    client.close()
+    try:
+        client = pymongo.MongoClient("mongodb://mongodb:27017/")
+        db = client["users"]
+        col = db["valentines"]
+        col.insert_one({"author": user_id})
+        client.close()
+    except Exception:
+        return -1
