6faf70bde1c0d53cf7657d0a7a5a93795430208e
yasamprom <stepa.strunkov@gmail.com>
2023-02-12 13:11:38 +0300

    db.py

--- db.py
+++ db.py
@@ -2,20 +2,18 @@ import pymongo
 
 
 async def add_user_to_db(user_id, unique=False):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    client = pymongo.MongoClient("mongodb://root:pass@127.0.0.1:27017")
     db = client["users"]
     col = db["user_ids"]
     if unique:
         x = col.find_one(user_id)
         if x:
             return
-    print("INSERT")
     col.insert_one({"user_id": user_id})
 
 
 async def add_valentine(user_id):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    client = pymongo.MongoClient("mongodb://root:pass@127.0.0.1:27017")
     db = client["users"]
     col = db["valentines"]
-    print("INSERT VAL")
     col.insert_one({"user_id": user_id})
